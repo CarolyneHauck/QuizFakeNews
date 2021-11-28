6e1d51f5d18950ff9a6b6dc9bb1d777eb84cc00a
CarolyneHauck <carolyne.carreira@gmail.com>
2021-11-28 13:09:26 -0300

    deixando tela de cadastro responsiva

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -77,115 +77,120 @@ class _LoginPageState extends State<LoginPage> {
             padding: EdgeInsets.only(top: 100),
             child: Form(
               key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset(
-                    "animations/news.json",
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    height: MediaQuery.of(context).size.width * 0.5,
-                  ),
-                  Text(
-                    titulo,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontFamily: 'NunitoRegular',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: TextFormField(
-                      controller: email,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
+              child: Expanded(
+                flex: 1,
+                child: SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset(
+                        "animations/news.json",
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: MediaQuery.of(context).size.width * 0.5,
                       ),
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Informe o email corretamente!';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(13),
-                    child: TextFormField(
-                      controller: senha,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Senha',
+                      Text(
+                        titulo,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontFamily: 'NunitoRegular',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Informa sua senha!';
-                        } else if (value.length < 6) {
-                          return 'Sua senha deve ter no mínimo 6 caracteres';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.blueGrey,
-                          textStyle: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold)),
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          if (isLogin) {
-                            login();
-                          } else {
-                            registrar();
-                          }
-                        }
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: (loading)
-                            ? [
-                                Padding(
-                                  padding: EdgeInsets.all(16),
-                                  child: SizedBox(
-                                    width: 24,
-                                    height: 24,
-                                    child: CircularProgressIndicator(
-                                      color: Colors.white,
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: TextFormField(
+                          controller: email,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe o email corretamente!';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(13),
+                        child: TextFormField(
+                          controller: senha,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Senha',
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informa sua senha!';
+                            } else if (value.length < 6) {
+                              return 'Sua senha deve ter no mínimo 6 caracteres';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueGrey,
+                              textStyle: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold)),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              if (isLogin) {
+                                login();
+                              } else {
+                                registrar();
+                              }
+                            }
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: (loading)
+                                ? [
+                                    Padding(
+                                      padding: EdgeInsets.all(16),
+                                      child: SizedBox(
+                                        width: 24,
+                                        height: 24,
+                                        child: CircularProgressIndicator(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    )
+                                  ]
+                                : [
+                                    Icon(Icons.check),
+                                    Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Text(
+                                        actionButton,
+                                        style: TextStyle(fontSize: 20),
+                                      ),
                                     ),
-                                  ),
-                                )
-                              ]
-                            : [
-                                Icon(Icons.check),
-                                Padding(
-                                  padding: EdgeInsets.all(16.0),
-                                  child: Text(
-                                    actionButton,
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ],
+                                  ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () => setFormAction(!isLogin),
-                    child: Text(
-                      toggleButton,
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 255, 0, 0),
-                        fontSize: 16,
+                      TextButton(
+                        onPressed: () => setFormAction(!isLogin),
+                        child: Text(
+                          toggleButton,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 0, 0),
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
